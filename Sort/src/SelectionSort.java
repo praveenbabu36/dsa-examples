84bc2788eb0c1c62ec306e311f09aa0a9c963ee2
Praveen <praveen.babu36@gmail.com>
2020-05-06 23:54:20 +0530

    Selection Sort Example

---
+++ Sort/src/SelectionSort.java
@@ -0,0 +1,42 @@
+public class SelectionSort
+{
+
+    public static void main(String args[])
+    {
+        int[] intArray  =   new int[]{1, 50, -1, 12, 0, 12};
+
+        // print the array - start
+        System.out.print("Start Array : " );
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.print(intArray[i] + " ");
+        }
+        System.out.println();
+
+        for(int unsortedPartIdx = intArray.length; unsortedPartIdx > 0; unsortedPartIdx--)
+        {
+            System.out.println("UnsortedPartIdx: " + unsortedPartIdx);
+
+            int largest = 0; // largest element first element
+            for (int i = 1; i < unsortedPartIdx; i++) {
+
+                System.out.println("   largest: " + largest  + " i:" + i);
+                if (intArray[i] > intArray[largest]) { //if element in the array is larger, change the larger variable
+                    largest = i;
+                }
+            }
+
+            // move the larger item to the unsorted partition index position
+            int temp = intArray[unsortedPartIdx - 1];
+            intArray[unsortedPartIdx - 1] = intArray[largest];
+            intArray[largest] = temp;
+
+            // print the array - end
+            System.out.print("End Array : " );
+            for (int i = 0; i < intArray.length; i++) {
+                System.out.print(intArray[i] + " ");
+            }
+            System.out.println();
+
+        } //end for outer
+    }
+}
