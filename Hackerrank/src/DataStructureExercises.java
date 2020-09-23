8e50bfd173c0f0a7a04f45e4a864099cab107041
praveen <praveen.babu36@gmail.com>
2020-09-23 19:59:46 +0530

    ArrayList Left Rotation -
    Signed-off-by: praveen <praveen.babu36@gmail.com>

--- Hackerrank/src/DataStructureExercises.java
+++ Hackerrank/src/DataStructureExercises.java
@@ -1,22 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataStructureExercises {
 
     public static void main(String args[]) {
 
         //https://www.hackerrank.com/challenges/arrays-ds/problem
-
+/*
         int[]  a = {1, 2, 3};
 
         a = reverseArray(a);
 
         for(int i=0; i<a.length; i++) {
             System.out.println(a[i]);
-        }
+        }*/
 
+        //https://www.hackerrank.com/challenges/array-left-rotation/problem
+        List<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+        arr.add(4);
+        arr.add(5);
+
+        int d = 2;
+
+        List<Integer> result = rotateLeft(d, arr);
+
+        for (int i = 0; i < result.size(); i++) {
+            System.out.println("result:" + result.get(i));
+        }
 
     }
 
+    //
+    public static List<Integer> rotateLeft(int d, List<Integer> arr) {
+        // Write your code here
+
+        //System.out.println(arr.get(i));
+        for(int i=0; i<d; i++) {
+            arr.add(arr.remove(0));
+        }
+
+        return arr;
+    }
+
     // Complete the reverseArray function below.
-    static int[] reverseArray(int[] a) {
+/*    static int[] reverseArray(int[] a) {
 
         int temp;
         for(int i=0, j=a.length-1; i<a.length/2; i++, j--) {
@@ -26,6 +56,5 @@ public class DataStructureExercises {
         }
 
         return a;
-    }
-
+    }*/
 }
