b049194f41c648a3ca0e84152dcd850371192cfe
praveen <praveen.babu36@gmail.com>
2020-09-24 13:25:25 +0530

    2D Array - Hourglass Sum
    Signed-off-by: praveen <praveen.babu36@gmail.com>

--- Hackerrank/src/DataStructureExercises.java
+++ Hackerrank/src/DataStructureExercises.java
@@ -15,6 +15,7 @@ public class DataStructureExercises {
             System.out.println(a[i]);
         }*/
 
+/*
         //https://www.hackerrank.com/challenges/array-left-rotation/problem
         List<Integer> arr = new ArrayList<>();
         arr.add(1);
@@ -29,12 +30,89 @@ public class DataStructureExercises {
 
         for (int i = 0; i < result.size(); i++) {
             System.out.println("result:" + result.get(i));
-        }
+        }*/
 
+        //https://www.hackerrank.com/challenges/30-2d-arrays/problem
+
+        int[][] arr = new int[6][6];
+
+/*        String[] inputStr = {"1 1 1 0 0 0",
+                             "0 1 0 0 0 0",
+                             "1 1 1 0 0 0",
+                             "0 9 2 -4 -4 0",
+                             "0 0 0 -2 0 0",
+                             "0 0 -1 -2 -4 0"};*/
+
+        String[] inputStr = {"-1 -1 0 -9 -2 -2",
+                             "-2 -1 -6 -8 -2 -5",
+                             "-1 -1 -1 -2 -3 -4",
+                             "-1 -9 -2 -4 -4 -5",
+                             "-7 -3 -3 -2 -9 -9",
+                             "-1 -3 -1 -2 -4 -5"};
+
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = inputStr[i].split(" ");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+        System.out.println(max2DArrayValue(arr));
     }
 
-    //
-    public static List<Integer> rotateLeft(int d, List<Integer> arr) {
+    public static int max2DArrayValue(int[][] arr) {
+
+       int row = 0;
+       int col = 0;
+       int max2DSum = 0;
+       int max2DSumTemp = 0;
+
+        //rows to traverse
+       for(int i=0; i<4; i++){
+
+           //column
+           for(int j=0; j<4; j++){
+               max2DSumTemp = 0;
+               System.out.println("---------------");
+               //3 rows
+               row = 0;
+               for (int ii = i; ii <= i + 2; ii++) {
+                   col = 0;
+                   System.out.println();
+                   //3 columns
+                   for (int jj = j; jj <= j + 2; jj++) {
+                        System.out.print(i + "," + j + "->:" + arr[ii][jj] + " ");
+                        if(row == 1) {
+                            if(col == 1)
+                                max2DSumTemp += arr[ii][jj];
+                        }else {
+                            max2DSumTemp += arr[ii][jj];
+                        }
+
+
+                        col++;
+                   }
+                   row++;
+               }
+
+               if(i == 0 && j ==0)
+                   max2DSum = max2DSumTemp;
+
+
+               if(max2DSumTemp > max2DSum)
+                   max2DSum = max2DSumTemp;
+
+               System.out.println("\n max2DSumTemp: " + max2DSumTemp + ", maxSum:" + max2DSum) ;
+           }
+       }
+
+        return max2DSum;
+    }
+
+    // rotateLeft Array
+/*    public static List<Integer> rotateLeft(int d, List<Integer> arr) {
         // Write your code here
 
         //System.out.println(arr.get(i));
@@ -43,7 +121,7 @@ public class DataStructureExercises {
         }
 
         return arr;
-    }
+    }*/
 
     // Complete the reverseArray function below.
 /*    static int[] reverseArray(int[] a) {
